86fa2378fd5508831d8f41986a8f648ede3ab0ec
Vladislav Balabanovich <vladislav.svoi@gmail.com>
2020-04-23 19:56:52 +0200

    Render elements in new way

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 const list = document.querySelector('.todo__list');
-const template = document.querySelector('#task-template').content.querySelector('.task');
+
+const createTaskElement = function() {
+	const markup = `
+		<li class="todo__item task">
+      <div class="task__info">
+        <p class="task__name"></p>
+      </div>
+      <div class="task__controls">
+        <button class="task__btn task__btn_edit" type="button"><img src="./images/edit-icon.svg" width="24" height="23" alt="Редактировать"></button>
+        <button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
+        <button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
+      </div>
+    </li>
+	`;
+
+	const element = document.createElement('div');
+	element.insertAdjacentHTML('afterbegin', markup);
+
+	return element.firstElementChild;
+};
+
+const renderSingleTask = function(name) {
+	const newTask = createTaskElement();
+	newTask.querySelector('.task__name').textContent = name;
+
+	list.appendChild(newTask);
+}
 
 // пройтись по массиву данных циклом
 tasks.forEach(function(task) {
-	const newTask = template.cloneNode(true);
-	newTask.querySelector('.task__name').textContent = task.name;
-
-	list.appendChild(newTask);
+	renderSingleTask(task.name);
 });
