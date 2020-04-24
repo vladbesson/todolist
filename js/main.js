8b0be5cf7cbd887fd9050926f4d866ddd532d557
Andrey Sysoev <andrey@sysoev.ru>
2020-04-24 12:57:11 +0300

    form was added

--- js/main.js
+++ js/main.js
@@ -1,65 +1,88 @@
 const list = document.querySelector('.todo__list');
 
-const createTaskElement = function() {
+function createTaskElement() {
 	const markup = `
 		<li class="todo__item task">
       <div class="task__info">
-        <p class="task__name"></p>
+				<p class="task__name"></p>
+				<form class="edit-form" name="edit">
+					<input type="text" name="task" class="input edit-form__input" required placeholder="Изменить задачу">
+					<button type="submit" class="button edit-form__button">
+				</form>
       </div>
       <div class="task__controls">
-        <button class="task__btn task__btn_edit" type="button"><img src="./images/edit-icon.svg" width="24" height="23" alt="Редактировать"></button>
-        <button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
-        <button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
+        <button class="task__btn task__btn_edit" type="button"><img class="task__img task__img_edit" src="./images/edit-icon.svg" width="24" height="23" alt="Редактировать"></button>
+        <button class="task__btn task__btn_copy" type="button"><img class="task__img task__img_copy" src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
+        <button class="task__btn task__btn_delete" type="button"><img class="task__img task__img_delete" src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
       </div>
     </li>
 	`;
 
 	const element = document.createElement('div');
+
 	element.insertAdjacentHTML('afterbegin', markup);
 
 	return element.firstElementChild;
 };
 
-const renderSingleTask = function(name) {
+function renderSingleTask(name) {
 	const newTask = createTaskElement();
+
 	newTask.querySelector('.task__name').textContent = name;
-
-	const deleteButton = newTask.querySelector('.task__btn_delete');
-	const copyButton = newTask.querySelector('.task__btn_copy');
-	const editButton = newTask.querySelector('.task__btn_edit');
-
-	deleteButton.addEventListener('click', function (evt) {
-		const task = evt.currentTarget.closest('.task');
-		list.removeChild(task);
-	});
-
-	editButton.addEventListener('click', function (evt) {
-		const text = prompt('Введите новый текст');
-		const task = evt.currentTarget.closest('.task');
-		task.querySelector('.task__name').textContent = text;
-	});
-
-	copyButton.addEventListener('click', function (evt) {
-		const task = evt.currentTarget.closest('.task');
-		const clonedTask = task.cloneNode(true);
-		task.after(clonedTask);
-	});
 
 	list.appendChild(newTask);
 };
 
-// пройтись по массиву данных циклом
-tasks.forEach(function(task) {
+tasks.forEach(function (task) {
 	renderSingleTask(task.name);
 });
 
 const form = document.querySelector('.todo__form');
 const input = document.querySelector('.todo__input');
 
-const onFormSubmitHandler = function (evt) {
+function onFormSubmitHandler(evt) {
 	evt.preventDefault();
 	renderSingleTask(input.value);
 	input.value = '';
 };
 
+function removeTask(evt) {
+	if (event.target.classList.contains('task__img_delete')) {
+		list.removeChild(event.target.closest('.task'));
+	}
+}
+
+function editTask(evt) {
+	if (event.target.classList.contains('task__img_edit')) {
+		const task = evt.target.closest('.task');
+		task.querySelector('.edit-form').classList.add('edit-form_active');
+		task.querySelector('.task__name').classList.add('task__name_inactive');
+		const editForm = document.forms.edit;
+		editForm.addEventListener('submit', editText);
+	}
+}
+
+function editText(evt) {
+	evt.preventDefault();
+
+	const task = evt.target.closest('.task');
+	const newTask = document.forms.edit.elements.task;
+
+	task.querySelector('.task__name').textContent = newTask.value;
+
+	task.querySelector('.edit-form').classList.remove('edit-form_active');
+	task.querySelector('.task__name').classList.remove('task__name_inactive');
+}
+
+function copyTask(evt) {
+	if (event.target.classList.contains('task__img_copy')) {
+		const task = evt.target.closest('.task');
+		const clonedTask = task.cloneNode(true);
+		task.after(clonedTask);
+	}
+}
+
 form.addEventListener('submit', onFormSubmitHandler);
+list.addEventListener('click', removeTask);
+list.addEventListener('click', editTask);
+list.addEventListener('click', copyTask);
