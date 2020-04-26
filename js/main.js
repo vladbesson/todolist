188d21d3082b9de6f4c7ba110f2ed23aee7e04d8
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-04-26 22:02:38 +0400

    Intermediate commit Day 2

--- js/main.js
+++ js/main.js
@@ -10,18 +10,21 @@ function createTaskElement() {
                 <p class="task__name"></p>
             </div>
             <div class="task__controls">
-                <button class="task__btn task__btn_edit" type="button"><img src="./images/edit-icon.svg" width="24" height="23" alt="Редактировать"></button>
-                <button class="task__btn task__btn_copy" type="button"><img src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
-                <button class="task__btn task__btn_delete" type="button"><img src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
-            </div>
-        </li>
-	`;
+                <button class="task__btn task__btn_edit" type="button"><img class="task__btn-image task__btn-image_edit"></button>
+                <button class="task__btn task__btn_copy" type="button"><img class="task__btn-image task__btn-image_copy"></button>
+                <button class="task__btn task__btn_delete" type="button"><img class="task__btn-image task__btn-image_delete"></button>
+                </div>
+                </li>
+                `;
 
     const element = document.createElement('div');
     element.insertAdjacentHTML('afterbegin', markup);
 
     return element.firstElementChild;
 };
+                // <button class="task__btn task__btn_edit" type="button"><img class="task__btn-image" src="./images/edit-icon.svg" width="24" height="23" alt="Редактировать"></button>
+                // <button class="task__btn task__btn_copy" type="button"><img class="task__btn-image" src="./images/duplicate-icon.svg" width="25" height="25" alt="Копировать"></button>
+                // <button class="task__btn task__btn_delete" type="button"><img class="task__btn-image" src="./images/delete-icon.svg" width="18" height="17" alt="Удалить"></button>
 
 // вызвать функцию создания пустой задачи, задать текст и добавить задачу на страницу
 function renderSingleTask(name) {
